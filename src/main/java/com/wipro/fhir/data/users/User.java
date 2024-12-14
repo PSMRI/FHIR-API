43d52e99a4b5fa8a21ca45b59ccad7071d4463c8
IN40068837 <IN40068837@L-156289031.wipro.com>
2024-12-14 12:50:53 +0530

    fixed issues

--- src/main/java/com/wipro/fhir/data/users/User.java
+++ src/main/java/com/wipro/fhir/data/users/User.java
@@ -1,9 +1,5 @@
 package com.wipro.fhir.data.users;
 
-import java.sql.Timestamp;
-
-import com.google.gson.annotations.Expose;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -18,57 +14,8 @@ import lombok.Data;
 public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Expose
 	@Column(name = "UserID")
 	private Long userID;
-
-	@Expose
-	@Column(name = "TitleID")
-	private Integer titleID;
-	@Expose
-	@Column(name = "FirstName")
-	private String firstName;
-	@Expose
-	@Column(name = "MiddleName")
-	private String middleName;
-	@Expose
-	@Column(name = "LastName")
-	private String lastName;
-
-	@Expose
-	@Column(name = "GenderID")
-	private Integer genderID;
-
-	@Expose
-	@Column(name = "MaritalStatusID")
-	private Integer maritalStatusID;
-
-	@Expose
-	@Column(name = "StatusID")
-	private Integer statusID;
-
-	@Expose
-	@Column(name = "DOB")
-	private Timestamp dOB;
-	@Expose
-	@Column(name = "DOJ")
-	private Timestamp dOJ;
-	@Expose
-	@Column(name = "QualificationID")
-	private Integer qualificationID;
-	@Expose
 	@Column(name = "userName")
 	private String userName;
-	@Expose
-	@Column(name = "Deleted", insertable = false, updatable = true)
-	private Boolean deleted;
-	@Expose
-	@Column(name = "CreatedBy")
-	private String createdBy;
-	@Column(name = "CreatedDate", insertable = false, updatable = false)
-	private Timestamp createdDate;
-	@Column(name = "ModifiedBy")
-	private String modifiedBy;
-	@Column(name = "LastModDate", insertable = false, updatable = false)
-	private Timestamp lastModDate;
 }
--- src/main/java/com/wipro/fhir/utils/JwtAuthenticationUtil.java
+++ src/main/java/com/wipro/fhir/utils/JwtAuthenticationUtil.java
@@ -69,17 +69,11 @@ public class JwtAuthenticationUtil {
 			}
 
 			String userId = claims.get("userId", String.class);
-			String tokenUsername = jwtUtil.extractUsername(jwtToken);
 
 			// Fetch user based on userId from the database or cache
 			User user = commonServiceImpl.getUserById(Long.parseLong(userId));
 			if (user == null) {
 				throw new Exception("Invalid User ID.");
-			}
-
-			// Check if the token's username matches the user retrieved by userId
-			if (!user.getUserName().equalsIgnoreCase(tokenUsername)) {
-				throw new Exception("JWT token and User ID mismatch.");
 			}
 
 			return true; // Valid userId and JWT token
