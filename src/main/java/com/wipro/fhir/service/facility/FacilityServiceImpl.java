2ecec4379a7e7a60df3fce35f662960b9dd28980
Helen Grace Karyamsetty <133211481+helenKaryamsetty@users.noreply.github.com>
2025-04-09 12:52:34 +0530

    Update getServices API to V3 (#70)
    * Modified tnxId keyword to txnId
    
    * Abha-address search API changes
    
    * added validation for multiple phraddress
    
    * Abha-address search response changes
    
    * Save facility id variable change chnages
    
    * Added check to avoid multiple save of single care-context
    
    * removed unused variables
    
    * Updated public key certificate API
    
    * Integrated Abha session v3 APIs and Profile login user verify APIs
    
    * committed coderabitai suggested
    
    * Revert "jwt implementation changes (#53)"
    
    This reverts commit 5e16b7d4ea27f66e91553d8bbb159648ff10a82b.
    
    * data Sync for FLW API changes
    
    * Reapply "jwt implementation changes (#53)"
    
    This reverts commit c38d0995378e199caf284b81b003959f96f8c974.
    
    * changed variable type default value as false
    
    * addition of isNewAbha in getHealthIdDetails API
    
    * Revert "jwt implementation changes (#53)"
    
    This reverts commit 5e16b7d4ea27f66e91553d8bbb159648ff10a82b.
    
    * verify abha issue with mobile number
    
    * Reapply "jwt implementation changes (#53)"
    
    This reverts commit 9f0372cb5394b7c5bd394f8f2812626dc2385d98.
    
    * lombok version changes
    
    * API to fetch beneficiary ID linked with HealthID
    
    * modified sending benIds
    
    * getServices API upgrade to V3
    
    * Updated environment files
    
    ---------
    
    Co-authored-by: KA40094929 <KA40094929@APL-5CD1394ZJT>
    Co-authored-by: Karyamsetty Helen Grace <ka40094929@wipro.com>
    Co-authored-by: KA40094929 <KA40094929@192.168.0.108>
    Co-authored-by: KA40094929 <KA40094929@192.168.0.107>
    Co-authored-by: KA40094929 <KA40094929@APL-5CD1394ZJT.wipro.com>

--- src/main/java/com/wipro/fhir/service/facility/FacilityServiceImpl.java
+++ src/main/java/com/wipro/fhir/service/facility/FacilityServiceImpl.java
@@ -1,14 +1,26 @@
 package com.wipro.fhir.service.facility;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
+import java.util.TimeZone;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
@@ -19,6 +31,7 @@ import com.wipro.fhir.data.mongo.care_context.SaveFacilityIdForVisit;
 import com.wipro.fhir.repo.healthID.BenHealthIDMappingRepo;
 import com.wipro.fhir.service.ndhm.Common_NDHMService;
 import com.wipro.fhir.service.ndhm.GenerateSession_NDHMService;
+import com.wipro.fhir.service.v3.abha.GenerateAuthSessionService;
 import com.wipro.fhir.utils.exception.FHIRException;
 import com.wipro.fhir.utils.http.HttpUtils;
 import com.wipro.fhir.utils.mapper.InputMapper;
@@ -27,9 +40,11 @@ public class FacilityServiceImpl implements FacilityService{
 	
 	@Value("${getAbdmFacilityServicies}")
 	private String getAbdmServicies;
-	
 	@Value("${abdmFacilityId}")
 	private String abdmFacilityId;
+	
+	@Value("${x-CM-ID}")
+	private String xCMId;
 	
 	@Autowired
 	private HttpUtils httpUtils;
@@ -38,7 +53,7 @@ public class FacilityServiceImpl implements FacilityService{
 	private Common_NDHMService common_NDHMService;
 	
 	@Autowired
-	private GenerateSession_NDHMService generateSession_NDHM;
+	private GenerateAuthSessionService generateAuthSessionService;
 	
 	@Autowired
 	private BenHealthIDMappingRepo benHealthIDMappingRepo;
@@ -47,13 +62,28 @@ public class FacilityServiceImpl implements FacilityService{
 	public String fetchRegisteredFacilities() throws FHIRException {
 		String res = null;
 		List<HashMap<String,Object>> list = new ArrayList<>();
+		RestTemplate restTemplate = new RestTemplate();
 		HashMap<String,Object> map = new HashMap<>();
 		try {
-			String ndhmAuthToken = generateSession_NDHM.getNDHMAuthToken();
-			HttpHeaders headers = common_NDHMService.getHeaders(ndhmAuthToken);
-			ResponseEntity<String> responseEntity = httpUtils.getWithResponseEntity(getAbdmServicies, headers);
+			String abhaAuthToken = generateAuthSessionService.getAbhaAuthToken();
+			MultiValueMap<String, String> headers = new LinkedMultiValueMap<>();
+			headers.add("Content-Type", MediaType.APPLICATION_JSON.toString());
+			headers.add("REQUEST-ID", UUID.randomUUID().toString());
+
+			TimeZone tz = TimeZone.getTimeZone("UTC");
+			DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+			df.setTimeZone(tz);
+			String nowAsISO = df.format(new Date());
+			headers.add("TIMESTAMP", nowAsISO);
+			headers.add("X-CM-ID", xCMId);
+			headers.add("Authorization", abhaAuthToken);
+			
+			HttpEntity<String> httpEntity = new HttpEntity<>(headers);
+			ResponseEntity<String> responseEntity = restTemplate.exchange(getAbdmServicies, HttpMethod.GET,
+					httpEntity, String.class);
+			
 			String responseStrLogin = common_NDHMService.getBody(responseEntity);
-			if (responseStrLogin != null) {
+			if (responseEntity.getStatusCode() == HttpStatusCode.valueOf(200) && responseEntity.hasBody()) {
 				JsonObject jsnOBJ = new JsonObject();
 				JsonParser jsnParser = new JsonParser();
 				JsonElement jsnElmnt = jsnParser.parse(responseStrLogin);
