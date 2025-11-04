6d5298fcd41ee2a26d639c725eb122ebb3342ec3
Helen Grace Karyamsetty <133211481+helenKaryamsetty@users.noreply.github.com>
2025-11-04 16:04:24 +0530

    Simplify queries in CareContextRepo interface
    Removed unnecessary 'order by 1 desc' clause from queries.

--- src/main/java/com/wipro/fhir/repo/v3/careContext/CareContextRepo.java
+++ src/main/java/com/wipro/fhir/repo/v3/careContext/CareContextRepo.java
@@ -9,16 +9,16 @@ import com.wipro.fhir.data.request_handler.PatientEligibleForResourceCreation;
 
 public interface CareContextRepo extends CrudRepository<PatientEligibleForResourceCreation, BigInteger> {
 	
-	@Query(value="SELECT COUNT(*) FROM t_phy_vitals WHERE VisitCode = :visitCode order by 1 desc", nativeQuery = true)
+	@Query(value="SELECT COUNT(*) FROM t_phy_vitals WHERE VisitCode = :visitCode", nativeQuery = true)
 	public int hasPhyVitals(String visitCode);
 	
-	@Query(value="SELECT COUNT(*) FROM t_prescribeddrug WHERE VisitCode = :visitCode order by 1 desc", nativeQuery = true)
+	@Query(value="SELECT COUNT(*) FROM t_prescribeddrug WHERE VisitCode = :visitCode", nativeQuery = true)
 	public int hasPrescribedDrugs(String visitCode);
 	
-	@Query(value="SELECT COUNT(*) FROM t_lab_testorder WHERE VisitCode = :visitCode order by 1 desc", nativeQuery = true)
+	@Query(value="SELECT COUNT(*) FROM t_lab_testorder WHERE VisitCode = :visitCode", nativeQuery = true)
 	public int hasLabtestsDone(String visitCode);
 	
-	@Query(value="SELECT COUNT(*) FROM t_childvaccinedetail1 WHERE VisitCode = :visitCode order by 1 desc", nativeQuery = true)
+	@Query(value="SELECT COUNT(*) FROM t_childvaccinedetail1 WHERE VisitCode = :visitCode", nativeQuery = true)
 	public int hasVaccineDetails(String visitCode);
 	
 }
