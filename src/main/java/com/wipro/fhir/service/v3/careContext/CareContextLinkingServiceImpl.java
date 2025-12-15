513a135be3070cbe5cba2ed3f1af379781c5086f
Helen Grace Karyamsetty <133211481+helenKaryamsetty@users.noreply.github.com>
2025-12-16 01:09:24 +0530

    Fix display setting for patient care context

--- src/main/java/com/wipro/fhir/service/v3/careContext/CareContextLinkingServiceImpl.java
+++ src/main/java/com/wipro/fhir/service/v3/careContext/CareContextLinkingServiceImpl.java
@@ -267,7 +267,7 @@ public class CareContextLinkingServiceImpl implements CareContextLinkingService 
 					cc.add(careContexts);
 
 					patient.setReferenceNumber(addCareContextRequest.getVisitCode());
-					patient.setDisplay(hiType + " care context of "
+					patient.setDisplay(addCareContextRequest.getVisitCategory() + " - " + hiType + " care context of "
 							+ addCareContextRequest.getAbhaNumber());
 					patient.setCount(1);
 					patient.setCareContexts(cc);
